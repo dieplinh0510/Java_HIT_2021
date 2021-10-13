b2b292242e02208c30dfcbee207e149a240547d5
dieplinh0510 <dieplinh1005.dh@gmail.com>
2021-10-13 21:31:07 +0700

    BTVN buoi 1

---
+++ BTVN buoi 1/src/bai2/RunMain.java
@@ -0,0 +1,28 @@
+package bai2;
+
+public class RunMain {
+    public static void main(String[] args) {
+        System.out.println("*       *     *    * * * *   * * * *    *   *");
+        System.out.println("|       |    / \\   |      *  |       *   \\ /");
+        System.out.println("* ----- *   *---*  * * * *   * * * *      *");
+        System.out.println("|       |  /     \\ |         |            |");
+        System.out.println("*       * *       **         *            *");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println("*    *  ******  *       *     *");
+        System.out.println("| \\  |  |        \\    / \\    /");
+        System.out.println("*  * *  ******    *   *  *  *");
+        System.out.println("|   \\|  |          \\/     \\/");
+        System.out.println("*    *  ******      *     *");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println("*   *  ******      *    * * * *");
+        System.out.println(" \\ /   |          / \\   |       *");
+        System.out.println("  *    ******    *---*  * * * *");
+        System.out.println("  |    |        /     \\ |     \\ ");
+        System.out.println("  *    ******  *       **      *");
+
+    }
+}
